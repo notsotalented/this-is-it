ddd61e97dfc6e75d9c27f97d5e1ef63078e21ab6
notsotalented <foxnguyenb@gmail.com>
2023-12-05 16:26:37 +0700

    EOD

--- app/Containers/User/Models/User.php
+++ app/Containers/User/Models/User.php
@@ -19,80 +19,89 @@ use Illuminate\Notifications\Notifiable;
 class User extends UserModel implements ChargeableInterface
 {
 
-    use ChargeableTrait;
-    use AuthorizationTrait;
-    use Notifiable;
-    use CrudTrait;
+  use ChargeableTrait;
+  use AuthorizationTrait;
+  use Notifiable;
+  use CrudTrait;
 
-    /**
-     * The database table used by the model.
-     *
-     * @var string
-     */
-    protected $table = 'users';
+  /**
+   * The database table used by the model.
+   *
+   * @var string
+   */
+  protected $table = 'users';
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-        'device',
-        'platform',
-        'gender',
-        'birth',
-        'social_provider',
-        'social_token',
-        'social_refresh_token',
-        'social_expires_in',
-        'social_token_secret',
-        'social_id',
-        'social_avatar',
-        'social_avatar_original',
-        'social_nickname',
-        'confirmed',
-        'is_client',
-    ];
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = [
+    'name',
+    'email',
+    'password',
+    'device',
+    'platform',
+    'gender',
+    'birth',
+    'social_provider',
+    'social_token',
+    'social_refresh_token',
+    'social_expires_in',
+    'social_token_secret',
+    'social_id',
+    'social_avatar',
+    'social_avatar_original',
+    'social_nickname',
+    'confirmed',
+    'is_client',
+  ];
 
-    protected $casts = [
-        'is_client' => 'boolean',
-        'confirmed' => 'boolean',
-    ];
+  protected $casts = [
+    'is_client' => 'boolean',
+    'confirmed' => 'boolean',
+  ];
 
-    /**
-     * The dates attributes.
-     *
-     * @var array
-     */
-    protected $dates = [
-        'created_at',
-        'updated_at',
-        'deleted_at',
-    ];
+  /**
+   * The dates attributes.
+   *
+   * @var array
+   */
+  protected $dates = [
+    'created_at',
+    'updated_at',
+    'deleted_at',
+  ];
 
-    /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+  /**
+   * The attributes excluded from the model's JSON form.
+   *
+   * @var array
+   */
+  protected $hidden = [
+    'password',
+    'remember_token',
+  ];
 
-    public function paymentAccounts()
-    {
-        return $this->hasMany(PaymentAccount::class);
+  public function paymentAccounts()
+  {
+    return $this->hasMany(PaymentAccount::class);
+  }
+
+  public function ownProducts()
+  {
+    return $this->hasMany(Product::class, 'user_id', 'id');
+  }
+
+  public function ownAccounts()
+  {
+    return $this->hasMany(Account::class, 'belongs_to', 'id');
+  }
+
+  public function getPhoto()
+  {
+    if ($this->social_avatar) {
+      return '/storage/uploads/photos/' . $this->social_avatar;
     }
-
-    public function ownProducts() {
-        return $this->hasMany(Product::class, 'user_id', 'id');
-    }
-
-    public function ownAccounts() {
-      return $this->hasMany(Account::class, 'belongs_to', 'id');
   }
 }
